cd8b704290891ebcc092e68c6d1ed481b9714199
Apoorv Kothari <akothari@cloudflare.com>
2025-02-22 23:11:46 -0800

    hkdf impl

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+use aws_lc_rs::hkdf::KeyType;
+use aws_lc_rs::hmac;
+
+const ALGO: hmac::Algorithm = hmac::HMAC_SHA256;
+
+fn main() {
+    println!("Hello, world!");
+
+    let ikm = Ikm { data: vec![1, 2] };
+    let info = "Test".to_string();
+    let len = 10;
+    let salt = vec![];
+
+    let prk = extract(salt, ikm);
+    let _okm = expand(prk, info, len);
+}
+
+// HKDF-Extract(salt, IKM) -> PRK
+//
+// Inputs:
+//   salt     optional salt value (a non-secret random value);
+//        if not provided, it is set to a string of HashLen zeros.
+//   IKM      input keying material
+//
+// Output:
+//   PRK      a pseudorandom key (of HashLen octets)
+//
+// The output PRK is calculated as follows:
+//   PRK = HMAC-Hash(salt, IKM)/
+fn extract(salt: Vec<u8>, ikm: Ikm) -> Prk {
+    let key = hmac::Key::new(ALGO, &salt);
+    let data = hmac::sign(&key, &ikm.data);
+    Prk { data }
+}
+
+// HKDF-Expand(PRK, info, L) -> OKM
+// Options:
+//   Hash   a hash function; HashLen denotes the length of the
+//          hash function output in octets
+//
+// Inputs:
+//   PRK    a pseudorandom key of at least HashLen octets
+//          (usually, the output from the extract step)
+//   info   optional context and application specific information
+//          (can be a zero-length string)
+//   L      length of output keying material in octets
+//          (<= 255*HashLen)
+//
+// Output:
+//   OKM      output keying material (of L octets)
+//
+// The output OKM is calculated as follows:
+//    N = ceil(L/HashLen)
+//    T = T(1) | T(2) | T(3) | ... | T(N)
+//    OKM = first L octets of T
+//
+//    where:
+//    T(0) = empty string (zero length)
+//    T(1) = HMAC-Hash(PRK, T(0) | info | 0x01)
+//    T(2) = HMAC-Hash(PRK, T(1) | info | 0x02)
+//    T(3) = HMAC-Hash(PRK, T(2) | info | 0x03)
+//    ...
+//
+// (where the constant concatenated to the end of each T(n) is a single octet.)
+fn expand(prk: Prk, info: String, len: usize) -> Okm {
+    let mut okm: Vec<u8> = Vec::new();
+
+    let n: u64 = (len as f64 / ALGO.len() as f64).ceil() as u64;
+    assert!(n < u8::MAX.into());
+    let n = n as u8;
+
+    let mut prev_t: Vec<u8> = vec![];
+    for i in 1..=n {
+        let hmac_data = [&prev_t, info.as_bytes(), &[i]].concat();
+        let key = hmac::Key::new(ALGO, prk.data.as_ref());
+        let t = hmac::sign(&key, &hmac_data);
+        prev_t = t.as_ref().to_vec();
+
+        okm.extend(t.as_ref());
+    }
+
+    Okm { data: okm }
+}
+
+struct Ikm {
+    data: Vec<u8>,
+}
+
+struct Prk {
+    data: hmac::Tag,
+}
+
+struct Okm {
+    data: Vec<u8>,
+}
